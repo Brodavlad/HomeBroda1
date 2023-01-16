4d8bffe30e86e688afa88ef215e83ba5645be1ac
Brodavlad <75326742+Brodavlad@users.noreply.github.com>
2023-01-16 16:29:57 +0200

    Update lab1script.sh

--- lab1script.sh
+++ lab1script.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-aws s3api create-bucket --bucket broda.bucket --region us-east-1                              
 
-aws s3api put-bucket-policy --bucket broda.bucket --policy file://policy_s3.json         
+aws s3api create-bucket --bucket broda.bucket.lab1 --region us-east-1
 
-aws s3 sync ./ s3://broda.bucket/                                                             
+aws s3api put-bucket-policy --bucket broda.bucket.lab1 --policy file://policy_s3.json         
 
-aws s3 website s3://broda.bucket/ --index-document index.html --error-document error.html
+aws s3 sync ./ s3://broda.bucket.lab1/                                                             
 
-aws s3 presign s3://broda.bucket/index.html
-aws s3 presign s3://broda.bucket/error.html
+aws s3 website s3://broda.bucket.lab1/ --index-document index.html --error-document error.html
+
+aws s3 presign s3://broda.bucket.lab1/index.html
+aws s3 presign s3://broda.bucket.lab1/error.html
 
 ## update (small) pause to have enough time to check url`s
 sleep 30
