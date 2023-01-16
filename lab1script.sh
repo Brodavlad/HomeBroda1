645d368bc3ebe482cd573e5294b71e1016452c74
Brodavlad <75326742+Brodavlad@users.noreply.github.com>
2023-01-16 16:17:57 +0200

    Update lab1script.sh

--- lab1script.sh
+++ lab1script.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-aws s3 mb s3://broda.bucket
-aws s3api put-bucket-policy --bucket broda.bucket --policy file://bucket_policy.json 
-aws s3 sync D://html.aws s3://broda.bucket
+aws s3api create-bucket --bucket broda.bucket --region us-east-1                              
+
+aws s3api put-bucket-policy --bucket broda.bucket --policy file://policy_s3.json         
+
+aws s3 sync ./ s3://broda.bucket/                                                             
+
 aws s3 website s3://broda.bucket/ --index-document index.html --error-document error.html
+
 aws s3 presign s3://broda.bucket/index.html
 aws s3 presign s3://broda.bucket/error.html
 
